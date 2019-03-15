fe983a301fd09d63205d02400c373f5135e67281
ashus-13 <ashutosh.soccer@gmail.com>
2019-03-15 15:54:40 +0530

    Link list with positions

---
+++ link_list_pos.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int data;
+	struct Node* next;
+};
+
+struct Node* head;
+void insert(int data, int n)
+{
+	struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+	temp->data = data;
+	temp->next = NULL;
+	if(n==1)
+	{
+		temp->next = head;
+		head = temp;
+		return ;
+	}
+
+	struct Node* temp1= head;
+	for(int i=0;i<(n-2);i++)
+	{
+		temp1=temp1->next;
+	}
+
+	temp->next = temp1->next;
+	temp1->next = temp;
+}
+ void show()
+{
+	struct Node* temp = head;
+	while(temp!=NULL)
+	{	
+		printf("%d ", temp->data);
+		temp =temp->next;
+	}
+
+}
+int main()
+{
+	head=NULL;
+	insert(2,1);
+	insert(3,2);
+	insert(4,3);
+	insert(6,1);
+	show();
+	return 0;
+}
